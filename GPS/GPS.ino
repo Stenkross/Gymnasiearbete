bbc23e8c6d609aa9299015f773dd127f0a5e598c
Stenkross <Tandkrok@gmail.com>
2025-11-15 12:51:54 +0100

    GPS koden
    GPS koden färdig och fungerande

--- GPS/GPS.ino
+++ GPS/GPS.ino
@@ -1,9 +1,24 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <TinyGPSPlus.h>
 
+TinyGPSPlus gps;
+
+void setup() {
+  Serial.begin(115200);
+  delay(500);
+
+  Serial1.begin(9600);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  while (Serial1.available()) {
+    gps.encode(Serial1.read());
+  }
 
+  if (gps.location.isUpdated()) {
+    Serial.print("Latitud: ");
+    Serial.println(gps.location.lat(), 6);
+    Serial.print("Longitud: ");
+    Serial.println(gps.location.lng(), 6);
+    Serial.println("---");
+  }
 }
