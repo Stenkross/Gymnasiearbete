7af2ea55a1f910d79f40d991379b3a24b198f615
Thamska <165489803+HugoUddmar@users.noreply.github.com>
2025-11-22 13:59:14 +0100

    gps + satellit + kryption

--- Kodpasatellit/Kodpasatellit.ino
+++ Kodpasatellit/Kodpasatellit.ino
@@ -5,13 +5,12 @@
 #include <RH_RF69.h>
 #include <Wire.h>
 #include <SD.h>
-#include <Adafruit_MPU6050.h>
 #include <Adafruit_Sensor.h>
 #include <SFE_BMP180.h>
 #include <TinyGPSPlus.h>
 
 #define RF69_FREQ   433.0
-#define RFM69_CS    5
+#define RFM69_CS    13
 #define RFM69_INT   24
 #define RFM69_RST   25
 #define LED         10
@@ -64,7 +63,11 @@ void setup() {
   rf69.setTxPower(20, true);
   Serial.print("RFM69 @ "); Serial.print(RF69_FREQ); Serial.println(" MHz");
 
-  rf69.setModemConfig(RH_RF69::FSK_Rb9_6Fd19_2); // kanske ha kvar
+  uint8_t key[] = { 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08,
+                    0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
+  rf69.setEncryptionKey(key);
+
+  rf69.setModemConfig(RH_RF69::FSK_Rb9_6Fd19_2); // kanske ha kva
 
   //Sen initieras SD-kortet
 
@@ -109,7 +112,9 @@ void setup() {
 
 void loop() {
   while (Serial1.available()) {
-    gps.encode(Serial1.read());
+    char c = Serial1.read();
+    Serial.write(c);
+    gps.encode(c);
   }
 
   //Tryck och temperatur
